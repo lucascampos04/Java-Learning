e8518339437a0d9fa7fceef6eefcbaf6a4d7211d
Lucascampos04 <camposdlucasoli@gmail.com>
2023-06-02 20:03:53 -0800

    or

---
+++ aulas/For.java
@@ -0,0 +1,11 @@
+package aulas;
+
+public class For {
+    public static void main(String[] args){
+        int n = 0;
+
+        for (n=1 n <= 10; n++){
+            System.out.println(": " + n);
+        }
+    }
+}
--- aulas/ifElse.java
+++ aulas/ifElse.java
@@ -2,15 +2,7 @@ package aulas;
 
 public class ifElse{
     public static void main(String[] args){
-        String nome = "Lucas";
         int idade = 18;
-        int i = 0;
-
-        for (i=0; i <= 5; i++) {
-            System.out.println(i);
-        }
-
-        System.out.println("Seu nome é :" + nome);
 
         if (idade >= 18){
             System.out.println("Voce é maior de idade");;
