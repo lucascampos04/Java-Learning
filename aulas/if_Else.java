d940e2433a9eb6087b3519ba50d3a5a9b3a74170
Lucascampos04 <camposdlucasoli@gmail.com>
2023-06-04 18:45:17 -0800

    Switch Case

--- aulas/if_Else.java
+++ aulas/if_Else.java
@@ -1,16 +1,16 @@
-package aulas;
+// package aulas;
 
-public class if_Else{
-    public static void main(String[] args){
-        int nota = 8;
+// public class if_Else{
+//     public static void main(String[] args){
+//         int nota = 4;
 
-        if (nota >= 8) {
-            System.out.println("Aluno aprovado");
-        } else if (nota <= 5) {
-            System.out.println("Aluno com direito de recuperação");
-        } 
-        else {
-            System.out.println("Aluno reprovado, sem direito a recuperação");
-        }
-    }    
-}
+//         if (nota >= 8) {
+//             System.out.println("Aluno aprovado");
+//         } else if (nota <= 5) {
+//             System.out.println("Aluno com direito de recuperação");
+//         } 
+//         else {
+//             System.out.println("Aluno reprovado, sem direito a recuperação");
+//         }
+//     }    
+// }
