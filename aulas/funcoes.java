47a7bd24c8f34eb8f4c61c09e34a56431f597166
Lucascampos04 <camposdlucasoli@gmail.com>
2023-06-06 17:00:36 -0800

    funções

---
+++ aulas/funcoes.java
@@ -0,0 +1,22 @@
+package aulas;
+
+public class funcoes {
+    public static void main(String[] args){
+        String nome = "Lucas";
+        int idade = 18;
+        saudacao(nome, idade);
+
+        int resultado = soma(2, 5);
+        System.out.println(resultado);
+
+    }
+
+    public static void saudacao(String nome, int idade){
+        System.out.println("hello, " + nome + " Voce tem " + idade + " anos ?");
+    }
+
+    public static int soma(int a, int b){
+        return a + b;
+    }
+}
+
