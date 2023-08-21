53ef4e0acd1f2e19a18d7e8c428efb433a803cd9
lucascampos04 <camposdlucasoli@gmail.com>
2023-08-21 16:20:54 -0300

    Objeto funcionario

---
+++ Funcionarios/Objetos/Funcionarios.java
@@ -0,0 +1,27 @@
+package Funcionarios.Objetos;
+
+public class Funcionarios {
+    String nome;
+    String cargo;
+
+    public Funcionarios(String nome, String cargo) {
+        this.nome = nome;
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
