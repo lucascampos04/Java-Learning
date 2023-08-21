5a6a6c168c3415724848577c8f128ae9795d1620
lucascampos04 <camposdlucasoli@gmail.com>
2023-08-21 16:21:36 -0300

    Objeto salario

---
+++ Funcionarios/Objetos/Salario.java
@@ -0,0 +1,17 @@
+package Funcionarios.Objetos;
+
+public class Salario {
+    double salario;
+    public Salario(double salario) {
+        this.salario = salario;
+    }
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
